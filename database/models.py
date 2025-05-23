3109fcc0bda8d4fa2a4c734396892c7f6dd4f49f
mewtek <mewtek@icloud.com>
2025-05-22 18:53:18 -0700

    Create database model for Ad Crawls

--- database/models.py
+++ database/models.py
@@ -1,10 +1,10 @@
 import uuid
 from datetime import datetime
 from typing import Annotated,List
-from sqlmodel import Field,Session,SQLModel,Column,ARRAY,Text
+from sqlmodel import Field,Session,SQLModel,Column,ARRAY,Text,UUID
 
 
-class WXStars(SQLModel, table=True):
+class WXStar(SQLModel, table=True):
     __tablename__ = "wxstars"
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(default="PENDING")
@@ -14,3 +14,16 @@ class WXStars(SQLModel, table=True):
     gfxpkg_lf: str = Field(default=None, nullable=True)
     gfxpkg_ldl: str = Field(default=None, nullable=True)
     ip_addr: str = Field(default="127.0.0.1", nullable=False)   # This can also be updated automatically
+    data_port: str = Field(default="7777", nullable=False)
+    data_port_pri: str = Field(default="7788", nullable=False)
+    
+    
+class AdCrawl(SQLModel, table=True):
+    __tablename__ = "adcrawls"
+    id: uuid.UUID = Field(default_factory=uuid.uuid3, primary_key=True)
+    stars: List[uuid.UUID] = Field(sa_column=Column(ARRAY(UUID)))
+    start_date: datetime
+    end_date: datetime
+    crawl: str
+    
+
