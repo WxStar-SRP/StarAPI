c0d5e14fa001c3ac62dd5a1467770bbaa194483d
mewtek <april.p@mistwx.com>
2025-06-03 02:41:36 -0700

    Add routes for headend registration and information

--- routers/headends.py
+++ routers/headends.py
@@ -1,6 +1,38 @@
+import uuid
 from fastapi import APIRouter, HTTPException
+from dependencies import SessionDep
+from sqlmodel import select
+from database.db_models import Headend
+from models.post import HeadendIn
 
 router = APIRouter(
     prefix = "/headend",
     tags = ["Headend Management"]
 )
+
+@router.post("/register", status_code=201)
+async def register_new_headend(headend: HeadendIn, session: SessionDep):
+    new_headend = Headend(
+        name = headend.name,
+        msocode = headend.msocode
+    )
+
+    session.add(new_headend)
+    session.commit()
+    session.refresh(new_headend)
+
+    return new_headend
+
+
+@router.get("/{headend_uuid}", status_code=200)
+async def get_headend_info(headend_uuid: uuid.UUID, session: SessionDep):
+    try:
+        headend = session.exec(select(Headend).where(Headend.id == headend_uuid)).one()
+
+        return headend
+
+    except: 
+        raise HTTPException(
+            status_code=404,
+            detail="Headend not found"
+        )
